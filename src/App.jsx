2723b5a57cba86c37a268a37211aa173be2151a7
yeamim11 <abubokorsiddikyeamim7809@gmail.com>
2025-07-07 21:35:03 +0600

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-
 import React from 'react'
+import UseEffect from './Component/UseEffect/UseEffect'
 
 const App = () => {
   return (
     <div >
       <h1 className='text-white bg-black w-full text-center text-5xl p-6'>Welcome to My React App</h1>
+      <UseEffect/>
     </div>
   )
 }
---
+++ src/Component/UseEffect/UseEffect.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react'
+
+const UseEffect = () => {
+    const [count, setCount] = useState(0);
+    const [loaging, setLoading] = useState(false);
+
+    // const handel = () => {
+    //     setCount((count) => count + 1); 
+    // } 
+    // ! button ar moddha funtion call korle useEffect run hobe na, karon useEffect ta component render er por run hoy
+
+    useEffect(() => {
+        console.log("useEffect run");
+        // ! useEffect er moddha jodi kono function call kori tahole seta component render er por run hobe
+        // ! useEffect er moddha jodi kono function call na kori tahole seta component render er por run hobe na
+    },[count])
+
+    return (
+        <div className='flex flex-col items-center justify-center bg-gray-100 p-6 gap-3'>
+            {console.log("render")}
+            <h1 className='text-4xl font-bold '>Counter : {count} </h1>
+            <button
+                className='p-3 text-3xl font-bold bg-black text-white  '
+                onClick={() => { setCount((count) => count + 1) }}>
+                +
+            </button>
+             <button
+                className='p-3 text-3xl font-bold bg-black text-white  '
+                onClick={() => {setLoading(!loaging)}}>
+                LOADING
+            </button>
+        </div>
+    )
+}
+
+export default UseEffect
