9ad04f81a4b99f835c8434642aaeb2adb60a3e4d
yeamim11 <abubokorsiddikyeamim7809@gmail.com>
2025-07-07 15:55:42 +0600

    asd

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div >
+      <h1 className='text-white bg-black w-full text-center text-5xl p-6'>Welcome to My React App</h1>
+    </div>
+  )
+}
+
+export default App
