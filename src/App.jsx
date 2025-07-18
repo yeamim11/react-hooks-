b4b277f478e345c885f9aadb739a2a6bf759c799
yeamim11 <abubokorsiddikyeamim7809@gmail.com>
2025-07-18 23:47:03 +0600

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import UseEffect from './Component/UseEffect/UseEffect'
 import From from './Component/hooksFrom/From'
+import DataFetch from './Component/UseEffect/DataFetch'
 
 const App = () => {
   return (
     <div >
       <h1 className='text-white bg-black w-full text-center text-5xl p-6'>Welcome to My React App</h1>
       {/* <UseEffect/> */}
-      <From/>
+      {/* <From/> */}
+      <DataFetch/>
     </div>
   )
 }
---
+++ src/Component/UseEffect/DataFetch.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DataFetch = () => {
+  return (
+    <div>DataFetch</div>
+  )
+}
+
+export default DataFetch
