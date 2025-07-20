5937353ed6423ffd163fa30a8349c52bc8064726
yeamim11 <abubokorsiddikyeamim7809@gmail.com>
2025-07-21 01:43:54 +0600

    asd

--- src/Component/UseEffect/DataFetch.jsx
+++ src/Component/UseEffect/DataFetch.jsx
@@ -1,68 +1,42 @@
-// import React, { useEffect, useState } from 'react'
-
-// const DataFetch = () => {
-//     const [photo, setPhoto] = useState(null)
-
-//     useEffect (() => {
-//         setTimeout(() => {
-//             fetch ("https://jsonplaceholder.typicode.com/comments")
-//         .then((res) =>{
-//             return res.json()
-//         })
-//         .then((data) => {
-//             setPhoto(data)
-//             console.log(photo);
-            
-//         })
-//         })
-        
-//     }, [])
- 
-//   return (
-//     <div>
-//         <h1 className='text-center text-black font-bold'>Comments</h1>
-//         {
-//             photo.map((com) => {
-//                 return <p>
-//                     {com.title}
-//                 </p>
-               
-//             })
-//         }
-//     </div>
-//   )
-// }
-
-// export default DataFetch
-
-
-
-
-
 import React, { useEffect, useState } from 'react'
 
 const DataFetch = () => {
     const [photo, setPhoto] = useState([])
+    const [error, setError] = useState(null)
 
     useEffect(() => {
-        fetch("https://jsonplaceholder.typicode.com/comments")
-            .then((res) => res.json())
+        fetch("https://jsonplaceholder.typicode.com/tos")
+            .then((res) => {
+                if (!res.ok) {
+                    throw Error("fetching is not successful")
+                }else{
+                    return res.json()
+
+                }
+                
+            })
             .then((data) => {
                 setPhoto(data)
+                setError(null)
             })
-            .catch((error) => console.error("Error fetching data:", error))
+            .catch((error) => {
+                setError(error.message)
+            })
     }, [])
 
     return (
         <div>
             <h1 className='text-center text-black font-bold'>Comments</h1>
+            {error && (
+                <p className="text-red-500 text-center font-semibold">{error}</p>
+            )}
             {
-                photo.slice(11 ,20).map((com) => ( 
-                    <div key={com.id} className=' flex flex-col justify-center items-start gap-4 p-5 bg-black '>
-                        <div className="bg-gray-800 p-6">
-                            <p className='bg-red-500 text-black'><strong>{com.name}</strong></p>
-                        <p className='text-white'>{com.body}</p>
-                        </div>
+                photo.slice(11, 20).map((com) => (
+                    <div key={com.id} className=' flex flex-col justify-center items-start gap-4 p-5 '>
+                     
+
+
+                        {com.title}
                     </div>
                 ))
             }
@@ -71,3 +45,15 @@ const DataFetch = () => {
 }
 
 export default DataFetch
+
+
+
+
+
+
+
+   {/* <div className="bg-gray-800 p-6">
+                            <p className='bg-white text-black'><strong>{com.name}</strong></p>
+                            <p className='text-white'>{com.body}</p>
+                        </div> */}
+                        {/* API */}  {/* https://jsonplaceholder.typicode.com/comments */}
